bb3f015bb10d2eeeff34a16e47b15ca1be61f295
Gabriel Correia <gabriel.correia@suporterenova.onmicrosoft.com>
2024-11-02 23:52:49 -0300

    iniciando projeto R-clinicas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormPageComponent } from './components/form-page/form-page.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FormPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
