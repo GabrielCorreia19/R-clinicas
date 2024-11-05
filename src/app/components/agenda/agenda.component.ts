fe822aeae13eba17e5753c94f5effd18f8213b36
Gabriel Correia <gabriel.correia@suporterenova.onmicrosoft.com>
2024-11-05 11:17:53 -0300

    feat: add comportamento nos botões tela Agenda

--- src/app/components/agenda/agenda.component.ts
+++ src/app/components/agenda/agenda.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../navbar/navbar.component";
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-agenda',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './agenda.component.html',
-  styleUrl: './agenda.component.css'
+  styleUrls: ['./agenda.component.css'],
 })
 export class AgendaComponent {
+  buttonGroups: { [key: number]: string } = {
+    0: 'Sim',
+    1: 'Sim',
+  };
 
+  constructor(private router: Router) {}
+
+  toggleButton(groupIndex: number, button: string) {
+    this.buttonGroups[groupIndex] = button;
+  }
+
+  backToHome() {
+    this.router.navigate(['/']);
+  }
 }
