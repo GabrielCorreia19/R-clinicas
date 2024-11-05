a14a307aa3a453f1e2b69efa5492ba692678b233
Gabriel Correia <gabriel.correia@suporterenova.onmicrosoft.com>
2024-11-05 10:41:40 -0300

    feat: ajustes select e remoção da div reconhecimento biometrico

--- src/app/components/form-page/form-page.component.ts
+++ src/app/components/form-page/form-page.component.ts
@@ -1,23 +1,30 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { MenuHeaderComponent } from "../menu-header/menu-header.component";
+import { MenuHeaderComponent } from '../menu-header/menu-header.component';
 
 @Component({
   selector: 'app-form-page',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, MenuHeaderComponent], 
+  imports: [CommonModule, NavbarComponent, MenuHeaderComponent],
   templateUrl: './form-page.component.html',
-  styleUrls: ['./form-page.component.css']
+  styleUrls: ['./form-page.component.css'],
 })
 export class FormPageComponent {
-  isNavbarVisible: boolean = false; 
+  isNavbarVisible: boolean = false;
   navBarStyle = {
-    position: 'absolute', 
+    position: 'absolute',
     zIndex: 100,
   };
 
+  constructor(private router: Router) {}
+
   toggleNavbar() {
     this.isNavbarVisible = !this.isNavbarVisible;
+  }
+
+  backToHome() {
+    this.router.navigate(['/']);
   }
 }
